3dd7a10621df57d1ed2f63b11fdfa12056ef591d
Erik Crevel <erik.crevel@ntlworld.com>
2017-02-04 00:23:25 +0000

    Add 2 extra program arguments following the file path to indicate translations to start and stop recording on.

--- words-per-stroke.py
+++ words-per-stroke.py
@@ -10,6 +10,17 @@ if len(sys.argv) < 2:
 with open(sys.argv[1]) as data_file:
     log = data_file.readlines()
 
+start_recording_translation = None
+stop_recording_translation = None
+
+if len(sys.argv) >= 3:
+    start_recording_translation = sys.argv[2]
+
+if len(sys.argv) >= 4:
+    stop_recording_translation = sys.argv[3]
+
+
+recording = start_recording_translation is None
 num_strokes = 0
 num_words = 0
 
@@ -31,23 +42,28 @@ for line in log:
         strokes = match.group("strokes").split(",")
         words = re.findall(r"[a-zA-Z\-']*", re.sub(r"{.*}", "", match.group("translation")))
 
-        entry_num_strokes = 0
-        entry_num_words = 0
+        if match.group("translation") == start_recording_translation:
+            recording = True
+        elif match.group("translation") == stop_recording_translation:
+            recording = False
+        elif recording:
+            entry_num_strokes = 0
+            entry_num_words = 0
 
-        for stroke in strokes:
-            if not stroke.isspace() and len(stroke) > 0:
-                entry_num_strokes += 1
+            for stroke in strokes:
+                if not stroke.isspace() and len(stroke) > 0:
+                    entry_num_strokes += 1
 
-        for word in words:
-            if not word.isspace() and len(word) > 0:
-                entry_num_words += 1
+            for word in words:
+                if not word.isspace() and len(word) > 0:
+                    entry_num_words += 1
 
-        if removal:
-            num_strokes -= entry_num_strokes
-            num_words -= entry_num_words
-        else:
-            num_strokes += entry_num_strokes
-            num_words += entry_num_words
+            if removal:
+                num_strokes -= entry_num_strokes
+                num_words -= entry_num_words
+            else:
+                num_strokes += entry_num_strokes
+                num_words += entry_num_words
 
 print("Strokes per word = "
     + str(float(num_strokes)/num_words)
